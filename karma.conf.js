55f87fdd54fa532114e7c36f229bc3ea7d9e8add
Akileng Isaac <isaacakileng56@gmail.com>
2017-02-03 15:59:48 +0300

    Review: fix spacing

--- karma.conf.js
+++ karma.conf.js
@@ -15,12 +15,12 @@ module.exports = function(config) {
     if (config.sourceMapping) {
         Object.assign(webpackConfig, {
             module: Object.assign(webpackConfig.module, {
-                postLoaders: [{
+                postLoaders: [ {
                     test: /\.ts$/,
                     loader: "istanbul-instrumenter",
                     include: path.resolve(__dirname, "src"),
                     exclude: /\.(spec)\.ts$/
-                }]
+                } ]
             })
         });
     }
